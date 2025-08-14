0c3be869a16d5e5c0caefafa8138804d5b74e035
tgltts <g82651504@gmail.com>
2025-08-14 16:40:26 -0300

    Create ex2.c

---
+++ ex2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+  float a;
+  float area;
+  float perimetro;
+
+  printf("Digite o valor do raio :");
+  sacanf("%f", &a);
+
+  area =  3.1415926 * (a*a);
+  perimetro = 2 *  3.1415926 * a; 
+
+  printf("O valor da área = %f e do perímetro = %f", area , perimetro);
+
+  return 0;
+}
